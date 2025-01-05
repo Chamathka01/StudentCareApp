7e910a3cdf22db9471340cbf25522de5168153f2
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-05 04:19:23 +0000

    Add header for login

--- StudentCareApp/components/Login.js
+++ StudentCareApp/components/Login.js
@@ -1,8 +1,12 @@
+import { View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 export default function Login(){
     return(
         <PaperProvider>
-
+            <View>
+            <Text variant = "headlineLarge">STUDENT LOGIN</Text>
+            </View>
+            
         </PaperProvider>
     );
 }
