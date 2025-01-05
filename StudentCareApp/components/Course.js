ec28b5abf37bbbcc288b289806d7e84640cb1839
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-05 17:35:14 +0000

    Add Headline for Course

---
+++ StudentCareApp/components/Course.js
@@ -0,0 +1,11 @@
+export default function Course() {
+    return(
+        <PaperProvider>
+        <ScrollView style={styles.container}>
+        <View style={styles.header}>
+             <Text style={styles.headerText}>UoV Student Care</Text>
+        </View>
+        </ScrollView>
+        </PaperProvider>
+    );
+}
