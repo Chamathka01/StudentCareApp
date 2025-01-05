4160823036ad4b062110adefabdd247659acafe5
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-05 17:35:53 +0000

    Add image

--- StudentCareApp/components/Course.js
+++ StudentCareApp/components/Course.js
@@ -5,6 +5,12 @@ export default function Course() {
         <View style={styles.header}>
              <Text style={styles.headerText}>UoV Student Care</Text>
         </View>
+        <View style={styles.imagecontain}>
+          <Image
+            source={require("../assets/uovlogo.png")}
+            style={styles.image}
+          />
+        </View>
         </ScrollView>
         </PaperProvider>
     );
