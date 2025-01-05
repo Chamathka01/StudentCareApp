0221301b6acd3b5f6168ecde76057026166e321e
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-05 17:01:42 +0000

    Use styles

--- StudentCareApp/components/Login.js
+++ StudentCareApp/components/Login.js
@@ -28,12 +28,13 @@ export default function Login(){
              </View>
              <View style={styles.imagecontain}>
              <Image
-            source={require("../assets/uovlogo.png")}
+            source={require("../assets/Logo.png")}
             style={styles.image}
           />
         </View>
-            <View style={styles.header}>
+            <View style={styles.container}>
             <Text variant = "headlineLarge">STUDENT LOGIN</Text>
+            <Divider/>
             </View>
             <View>
             <View style={styles.formcontainer}>
@@ -53,3 +54,38 @@ export default function Login(){
         </PaperProvider>
     );
 }
+
+const styles = StyleSheet.create({
+  container: { alignItems: "center" },
+  header: {
+    width: "100%",
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#8b008b",
+  },
+  headerText: {
+    fontSize: 24,
+    color: "#fff",
+    backgroundColor: "#8b008b",
+    fontWeight: "bold",
+  },
+  image: {
+    width: "60%",
+    height: 100,
+    resizeMode: "contain",
+    marginTop: 5,
+  },
+  imagecontain: { alignItems: "center" },
+  loginText: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  formcontainer: {
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  buttoncontainer: {
+    paddingTop: 20,
+  },
+});
