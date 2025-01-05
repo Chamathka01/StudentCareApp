c6760c9a1f8ba51d0c0d1bd4794daac4ce33bb8a
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-05 17:14:49 +0000

    Add useState

--- StudentCareApp/components/Profile.js
+++ StudentCareApp/components/Profile.js
@@ -1,9 +1,21 @@
-export default function Profile() {
+import { Image, ScrollView, StyleSheet, View } from "react-native";
+import {Button, Divider, PaperProvider, Text, TextInput } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Profile({ route }) {
+    const { studentdata } = route.params;
+    const navigate = useNavigation();
     return(
         <PaperProvider>
         <ScrollView>
             <View style={styles.header}>
             <Text style={styles.headerText}>UoV Student Care</Text>
+        </View>
+        <View style={styles.imagecontain}>
+          <Image
+            source={require("../assets/uovlogo.png")}
+            style={styles.image}
+          />
         </View>
         </ScrollView>   
         </PaperProvider>
