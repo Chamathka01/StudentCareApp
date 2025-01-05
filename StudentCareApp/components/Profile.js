d1b81bb1056eaee3a761ec77049c6ee46d57d0cf
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-05 17:12:10 +0000

    Added header for Profile.js

--- StudentCareApp/components/Profile.js
+++ StudentCareApp/components/Profile.js
@@ -1,5 +1,11 @@
 export default function Profile() {
     return(
-
+        <PaperProvider>
+        <ScrollView>
+            <View style={styles.header}>
+            <Text style={styles.headerText}>UoV Student Care</Text>
+        </View>
+        </ScrollView>   
+        </PaperProvider>
     );
 }
